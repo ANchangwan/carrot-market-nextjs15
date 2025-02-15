2f07fc9389849f961e4ad5fea23b5a45cdc3461c
waney <ssdii66@gmail.com>
2025-02-15 18:13:56 +0900

    git login complete

--- app/github/start/route.ts
+++ app/github/start/route.ts
@@ -1,14 +1,15 @@
+import {redirect} from "next/navigation";
 
- export function GET(){
+export function GET(){
     const baseUrl = "https://github.com/login/oauth/authorize";
     const params = {
         client_id: process.env.GITHUB_CLIENT_ID,
         scope:"read:user,user:email",
         allow_signup:true,
     }
+
     const formatterParams = new URLSearchParams(params).toString();
     const finalUrl = `${baseUrl}?${formatterParams}`;
 
-    return Response.redirect(finalUrl);
-
+    return redirect(finalUrl);
  }
