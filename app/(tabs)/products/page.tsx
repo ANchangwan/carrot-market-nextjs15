7952a424f0d91b840b205568cabab886acafad3d
waney <ssdii66@gmail.com>
2025-02-19 22:59:08 +0900

    skeleton - loading page

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,5 +1,12 @@
 
-export default function Products(){
+async function getProducts() {
+    return new Promise(resolve => {
+        setTimeout(resolve,10000)
+    })
+}
+
+export default async function Products(){
+    const products = await getProducts();
     return (
         <div>
             <h1 className="text-white text-4xl">Product!</h1>
